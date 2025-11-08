2aff7aa68b0e4aaf174086bb8691654b88a04ded
Eternal Dreamer <mike.cod013@gmail.com>
2025-11-08 14:53:45 +0100

    feat: transcript keyboard

--- src/input.py
+++ src/input.py
@@ -22,10 +22,75 @@ from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.styles import Style
 from prompt_toolkit.keys import Keys
 
+
 ALWAYS_VISIBLE = [" ", "-", "'"]
+
+# Grave, Acute, ^, ~, °
+ACCENT = {
+	"a": ("à", "á", "â", "ã", "ä", "å"),
+	"e": ("è", "é", "ê", "" "ë"),
+	"i": ("ì", "í", "î", "", "ï"),
+	"o": ("ò", "ó", "ô", "õ", "ö"),
+	"u": ("ù", "ú", "û", "", "ü"),
+	"y": ("ỳ", "ý", "ŷ", "", "ÿ"),
+	"n": ("", "", "", "ñ"),
+}
+TRANSCRIPT_CHAR = "<>&~:°"
+TRANSCRIPT_LAYOUT = {
+    "a": "а",
+    "b": "б",
+    "v": "в",
+    "g": "г",
+    "d": "д",
+    # "e": ("e", "ye"),
+	# ":": ("ё", "yo"),
+	"j": "ж",
+	"z": "з",
+	"i": "и",
+	"k": "к",
+	"l": "л",
+	"m": "м",
+	"n": "н",
+	"p": "п",
+	"r": "р",
+	"s": "с",
+	"u": "у",
+	"f": "ф",
+	"x": "х",
+	"\"": "ъ",
+	"'": "ь",
+	"e": "э",
+
+	"h": {
+		"c": ("ч", "ch"),
+		"s": ("ш", "sh"),
+		"h": ("щ", "sch")
+	},
+	"t": {
+		"t": "т",
+		"s": "ц"
+	},
+	"y": {
+		"e": "е",
+		"o": "ё",
+		"i": ("й", "yi"),
+		"y": ("ы", "y"),
+		"u": "ю",
+		"a": "я"
+	}
+}
+def transcript_get_char(d: str | tuple) -> str:
+	if isinstance(d, tuple):
+		return d[0]
+	return d
+
+TRANSCRIPT_LAYOUT_COUNTRY_CODE = ["uk", "ru"]
+DEFAULT_CHAR = -1
+
 
 def slot_input(from_colour: str, to_colour: str, from_text: str, to_text: str, prompt_text: str, length: int, filled_slots: dict = {}, newline: bool = True, expect_full_input: bool = False) -> str:
 	entered = ""
+	special_char = DEFAULT_CHAR
 
 	def autofill(res: str):
 		curr_len = len(res)
@@ -113,8 +178,35 @@ def slot_input(from_colour: str, to_colour: str, from_text: str, to_text: str, p
 	@kb.add(Keys.Any)
 	def _(event):
 		nonlocal entered
+		nonlocal special_char
 		if event.data not in ALWAYS_VISIBLE and len(entered) < length and event.data.isprintable():
-			entered += event.data
+			# print(to_text.upper(), TRANSCRIPT_LAYOUT_COUNTRY_CODE)
+			if to_text.lower() in TRANSCRIPT_LAYOUT_COUNTRY_CODE:
+				# print("Transcripting")
+				if special_char != DEFAULT_CHAR:
+					if event.data in TRANSCRIPT_LAYOUT[special_char]:
+						entered += transcript_get_char(TRANSCRIPT_LAYOUT[special_char][event.data.lower()])
+					special_char = DEFAULT_CHAR
+
+				elif event.data.lower() in TRANSCRIPT_LAYOUT:
+					if isinstance(TRANSCRIPT_LAYOUT[event.data.lower()], dict):
+						special_char = event.data.lower()
+					else:
+						entered += transcript_get_char(TRANSCRIPT_LAYOUT[event.data.lower()])
+				else:
+					entered += event.data
+
+			elif event.data in TRANSCRIPT_CHAR:
+				special_char = TRANSCRIPT_CHAR.index(event.data)
+				return
+
+			elif special_char != DEFAULT_CHAR:
+				# print(special_char)
+				if event.data in ACCENT and special_char < len(ACCENT[event.data]) and ACCENT[event.data][special_char]:
+					entered += ACCENT[event.data][special_char]
+				special_char = DEFAULT_CHAR
+			else:
+				entered += event.data
 			event.app.invalidate()
 			entered = autofill(entered)
 
